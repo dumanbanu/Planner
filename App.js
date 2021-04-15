aeebfc574f6a8b65afe0518be49f5d73da94bd5c
dumanbanu <duman.banu@hotmail.com>
2021-04-15 10:18:20 +0300

    banu

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAluQdAMT--lGmZPixU5JQrGvzlOGoJ9RA",
+  authDomain: "friendsplanner.firebaseapp.com",
+  databaseURL: "https://friendsplanner.firebaseio.com",
+  projectId: "friendsplanner",
+  storageBucket: "friendsplanner.appspot.com",
+  messagingSenderId: "541886107298",
+  appId: "1:541886107298:web:18834108e203dc28ebabeb"
+};
+firebase.initializeApp(firebaseConfig);
+
 
 export default function App() {
+
+  React.useEffect(() => {
+//ilk açıldığında yapılması istenilenler buraya yazılır
+}, []);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
